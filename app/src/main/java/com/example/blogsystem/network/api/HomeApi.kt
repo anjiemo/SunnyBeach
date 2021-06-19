a67274da1ae5959d27f9503bb47438b4e7514456
anjiemo <2695734816@qq.com>
2021-06-19 19:06:42 +0800

    优化了首页界面切换效果。
    Signed-off-by: anjiemo <Feng5201314>

--- app/src/main/java/com/example/blogsystem/network/api/HomeApi.kt
+++ app/src/main/java/com/example/blogsystem/network/api/HomeApi.kt
@@ -9,9 +9,24 @@ import retrofit2.http.Path
 
 interface HomeApi {
 
+    /**
+     * 根据分类id获取内容
+     */
+    @GET("${SUNNY_BEACH_BASE_URL}ct/content/home/recommend/{categoryId}/{page}")
+    suspend fun getCommendContentByCategoryId(
+        @Path("categoryId") categoryId: String,
+        @Path("page") page: Int
+    ): BaseResponse<ArticleInfo>
+
+    /**
+     * 获取推荐内容
+     */
     @GET("${SUNNY_BEACH_BASE_URL}ct/content/home/recommend/{page}")
     suspend fun getRecommendContent(@Path("page") page: Int): BaseResponse<ArticleInfo>
 
+    /**
+     * 获取分类列表
+     */
     @GET("${SUNNY_BEACH_BASE_URL}ct/category/list")
     suspend fun getCategories(): BaseResponse<HomeCategories>
 }
