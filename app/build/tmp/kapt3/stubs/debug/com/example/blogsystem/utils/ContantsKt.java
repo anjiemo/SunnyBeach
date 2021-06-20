68266ab5ee9363519703d7d123c2d7989cd055e6
anjiemo <Feng5201314>
2021-06-20 16:51:14 +0800

    添加了反馈吐槽界面。
    Signed-off-by: anjiemo <Feng5201314>

--- app/build/tmp/kapt3/stubs/debug/com/example/blogsystem/utils/ContantsKt.java
+++ app/build/tmp/kapt3/stubs/debug/com/example/blogsystem/utils/ContantsKt.java
@@ -2,7 +2,7 @@ package com.example.blogsystem.utils;
 
 import java.lang.System;
 
-@kotlin.Metadata(mv = {1, 4, 2}, bv = {1, 0, 3}, k = 2, d1 = {"\u0000\u0012\n\u0000\n\u0002\u0010\u000e\n\u0002\b\u0004\n\u0002\u0010\b\n\u0002\b\n\"\u000e\u0010\u0000\u001a\u00020\u0001X\u0086T\u00a2\u0006\u0002\n\u0000\"\u000e\u0010\u0002\u001a\u00020\u0001X\u0086T\u00a2\u0006\u0002\n\u0000\"\u000e\u0010\u0003\u001a\u00020\u0001X\u0086T\u00a2\u0006\u0002\n\u0000\"\u000e\u0010\u0004\u001a\u00020\u0001X\u0086T\u00a2\u0006\u0002\n\u0000\"\u000e\u0010\u0005\u001a\u00020\u0006X\u0086T\u00a2\u0006\u0002\n\u0000\"\u000e\u0010\u0007\u001a\u00020\u0001X\u0086T\u00a2\u0006\u0002\n\u0000\"\u000e\u0010\b\u001a\u00020\u0001X\u0086T\u00a2\u0006\u0002\n\u0000\"\u000e\u0010\t\u001a\u00020\u0001X\u0086T\u00a2\u0006\u0002\n\u0000\"\u000e\u0010\n\u001a\u00020\u0006X\u0086T\u00a2\u0006\u0002\n\u0000\"\u000e\u0010\u000b\u001a\u00020\u0001X\u0086T\u00a2\u0006\u0002\n\u0000\"\u000e\u0010\f\u001a\u00020\u0001X\u0086T\u00a2\u0006\u0002\n\u0000\"\u0011\u0010\r\u001a\u00020\u0001\u00a2\u0006\b\n\u0000\u001a\u0004\b\u000e\u0010\u000f\u00a8\u0006\u0010"}, d2 = {"APP_INFO_URL", "", "BASE_URL", "DEFAULT_AVATAR_URL", "DEFAULT_BANNER_URL", "DEFAULT_HTTP_OK_CODE", "", "DEFAULT_URL", "IS_FIRST", "SUNNY_BEACH_BASE_URL", "SUNNY_BEACH_HTTP_OK_CODE", "SUNNY_BEACH_USER_BASIC_INFO", "USER_SETTING", "VERIFY_CODE_URL", "getVERIFY_CODE_URL", "()Ljava/lang/String;", "app_debug"})
+@kotlin.Metadata(mv = {1, 4, 2}, bv = {1, 0, 3}, k = 2, d1 = {"\u0000\u0012\n\u0000\n\u0002\u0010\u000e\n\u0002\b\u0005\n\u0002\u0010\b\n\u0002\b\u000f\"\u000e\u0010\u0000\u001a\u00020\u0001X\u0086T\u00a2\u0006\u0002\n\u0000\"\u000e\u0010\u0002\u001a\u00020\u0001X\u0086T\u00a2\u0006\u0002\n\u0000\"\u000e\u0010\u0003\u001a\u00020\u0001X\u0086T\u00a2\u0006\u0002\n\u0000\"\u000e\u0010\u0004\u001a\u00020\u0001X\u0086T\u00a2\u0006\u0002\n\u0000\"\u000e\u0010\u0005\u001a\u00020\u0001X\u0086T\u00a2\u0006\u0002\n\u0000\"\u000e\u0010\u0006\u001a\u00020\u0007X\u0086T\u00a2\u0006\u0002\n\u0000\"\u000e\u0010\b\u001a\u00020\u0001X\u0086T\u00a2\u0006\u0002\n\u0000\"\u000e\u0010\t\u001a\u00020\u0001X\u0086T\u00a2\u0006\u0002\n\u0000\"\u000e\u0010\n\u001a\u00020\u0001X\u0086T\u00a2\u0006\u0002\n\u0000\"\u000e\u0010\u000b\u001a\u00020\u0001X\u0086T\u00a2\u0006\u0002\n\u0000\"\u000e\u0010\f\u001a\u00020\u0001X\u0086T\u00a2\u0006\u0002\n\u0000\"\u000e\u0010\r\u001a\u00020\u0001X\u0086T\u00a2\u0006\u0002\n\u0000\"\u000e\u0010\u000e\u001a\u00020\u0001X\u0086T\u00a2\u0006\u0002\n\u0000\"\u000e\u0010\u000f\u001a\u00020\u0001X\u0086T\u00a2\u0006\u0002\n\u0000\"\u000e\u0010\u0010\u001a\u00020\u0007X\u0086T\u00a2\u0006\u0002\n\u0000\"\u000e\u0010\u0011\u001a\u00020\u0001X\u0086T\u00a2\u0006\u0002\n\u0000\"\u000e\u0010\u0012\u001a\u00020\u0001X\u0086T\u00a2\u0006\u0002\n\u0000\"\u0011\u0010\u0013\u001a\u00020\u0001\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0014\u0010\u0015\u00a8\u0006\u0016"}, d2 = {"ADDRESS", "", "APP_INFO_URL", "BASE_URL", "DEFAULT_AVATAR_URL", "DEFAULT_BANNER_URL", "DEFAULT_HTTP_OK_CODE", "", "DEFAULT_URL", "HEADER_IMG_URL", "IS_FIRST", "MAKE_COMPLAINTS_URL", "NICK_NAME", "OPEN_ID", "QQ", "SUNNY_BEACH_BASE_URL", "SUNNY_BEACH_HTTP_OK_CODE", "SUNNY_BEACH_USER_BASIC_INFO", "USER_SETTING", "VERIFY_CODE_URL", "getVERIFY_CODE_URL", "()Ljava/lang/String;", "app_debug"})
 public final class ContantsKt {
     @org.jetbrains.annotations.NotNull()
     public static final java.lang.String USER_SETTING = "userSetting";
@@ -26,6 +26,18 @@ public final class ContantsKt {
     public static final java.lang.String APP_INFO_URL = "http://hm03097.h37.hmie.cn/blog/app/appconfig.json";
     @org.jetbrains.annotations.NotNull()
     public static final java.lang.String SUNNY_BEACH_USER_BASIC_INFO = "sunny_beach_user_basic_info";
+    @org.jetbrains.annotations.NotNull()
+    public static final java.lang.String MAKE_COMPLAINTS_URL = "https://support.qq.com/product/333302";
+    @org.jetbrains.annotations.NotNull()
+    public static final java.lang.String OPEN_ID = "open_id";
+    @org.jetbrains.annotations.NotNull()
+    public static final java.lang.String NICK_NAME = "nick_name";
+    @org.jetbrains.annotations.NotNull()
+    public static final java.lang.String HEADER_IMG_URL = "https://cdn.sunofbeaches.com/images/default_avatar.png";
+    @org.jetbrains.annotations.NotNull()
+    public static final java.lang.String ADDRESS = "address";
+    @org.jetbrains.annotations.NotNull()
+    public static final java.lang.String QQ = "qq_number";
     
     @org.jetbrains.annotations.NotNull()
     public static final java.lang.String getVERIFY_CODE_URL() {
