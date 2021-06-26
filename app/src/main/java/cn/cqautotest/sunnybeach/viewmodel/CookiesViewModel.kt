f7988cf33e8f5799e90ea1f1119f171649e0088a
anjiemo <2695734816@qq.com>
2021-06-26 14:12:13 +0800

    新增沉浸式状态栏，优化登录界面及登录流程。

--- app/src/main/java/cn/cqautotest/sunnybeach/viewmodel/CookiesViewModel.kt
+++ app/src/main/java/cn/cqautotest/sunnybeach/viewmodel/CookiesViewModel.kt
@@ -1,6 +1,7 @@
 package cn.cqautotest.sunnybeach.viewmodel
 
 import android.app.Application
+import android.webkit.CookieManager
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
 import cn.cqautotest.sunnybeach.app.AppApplication
@@ -11,8 +12,22 @@ class CookiesViewModel(application: Application) : AndroidViewModel(application)
 
     private val cookieDao = AppApplication.getDatabase().cookieDao()
 
+    /**
+     * 保存 Cookies
+     */
     fun save(cookieStoreSet: List<CookieStore>) = viewModelScope.launch {
+        // 把 Cookies 保存到存储 Cookie 的 Room 数据库中
         cookieDao.save(cookieStoreSet)
+        // 将 Cookie 同步到 WebView 中
+        val cm = CookieManager.getInstance()
+        cm.run {
+            for (cookieStore in cookieStoreSet) {
+                for (cookie in cookieStore.cookies) {
+                    setCookie(cookieStore.host, cookie.value)
+                }
+            }
+            flush()
+        }
     }
 
     fun getCookiesByHost(host: String) = cookieDao.getCookiesByHost(host)
