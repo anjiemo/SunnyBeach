82b48b2f4b00644c1c63f54244b8fe5bacaebc4f
anjiemo <2695734816@qq.com>
2021-07-11 21:36:11 +0800

    新增了鱼塘详情评论列表。

--- app/src/main/java/cn/cqautotest/sunnybeach/ui/adapter/FishPondDetailCommendListAdapter.kt
+++ app/src/main/java/cn/cqautotest/sunnybeach/ui/adapter/FishPondDetailCommendListAdapter.kt
@@ -1,15 +1,14 @@
 package cn.cqautotest.sunnybeach.ui.adapter
 
-import android.text.TextUtils
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.core.content.ContextCompat
 import cn.cqautotest.sunnybeach.R
 import cn.cqautotest.sunnybeach.model.Fish
+import cn.cqautotest.sunnybeach.model.FishPondRecommend
 import com.bumptech.glide.Glide
 import com.chad.library.adapter.base.BaseQuickAdapter
-import com.chad.library.adapter.base.module.DraggableModule
 import com.chad.library.adapter.base.module.LoadMoreModule
 import com.chad.library.adapter.base.viewholder.BaseViewHolder
 
@@ -17,19 +16,18 @@ import com.chad.library.adapter.base.viewholder.BaseViewHolder
  * author : A Lonely Cat
  * github : https://github.com/anjiemo/SunnyBeach
  * time   : 2021/7/11
- * desc   : 摸鱼话题列表适配器
+ * desc   : 摸鱼话题评论列表适配器
  */
-class FishPondListAdapter :
-    BaseQuickAdapter<Fish.FishItem, BaseViewHolder>(R.layout.fish_pond_list_item),
-    LoadMoreModule, DraggableModule {
-    override fun convert(holder: BaseViewHolder, item: Fish.FishItem) {
+class FishPondDetailCommendListAdapter: BaseQuickAdapter<FishPondRecommend.FishPondRecommendItem, BaseViewHolder>(
+    R.layout.fish_pond_detail_commend_list),
+    LoadMoreModule {
+    override fun convert(holder: BaseViewHolder, item: FishPondRecommend.FishPondRecommendItem) {
         holder.run {
             val flAvatarContainer = getView<View>(R.id.fl_avatar_container)
             val ivAvatar = getView<ImageView>(R.id.iv_fish_pond_avatar)
             val tvNickname = getView<TextView>(R.id.tv_fish_pond_nick_name)
             val tvDesc = getView<TextView>(R.id.tv_fish_pond_desc)
-            val tvContent = getView<TextView>(R.id.tv_fish_pond_content)
-            val tvLabel = getView<TextView>(R.id.tv_fish_pond_label)
+            val tvContent = getView<TextView>(R.id.tv_fish_pond_recommend_content)
             flAvatarContainer.background = if (item.vip) ContextCompat.getDrawable(
                 context,
                 R.drawable.avatar_circle_vip_ic
@@ -52,9 +50,6 @@ class FishPondListAdapter :
             tvNickname.text = item.nickname
             tvDesc.text = item.position
             tvContent.text = item.content
-            val topicName = item.topicName
-            tvLabel.visibility = if (TextUtils.isEmpty(topicName)) View.GONE else View.VISIBLE
-            tvLabel.text = topicName
         }
     }
 }
