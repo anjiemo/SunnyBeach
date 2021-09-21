951adb2484c2685df2cc70bd71fbd68855602c79
anjiemo <2695734816@qq.com>
2021-09-21 17:31:39 +0800

    “我的”界面用户信息回显。

--- app/src/main/java/cn/cqautotest/sunnybeach/ui/fragment/MyMeFragment.kt
+++ app/src/main/java/cn/cqautotest/sunnybeach/ui/fragment/MyMeFragment.kt
@@ -1,5 +1,6 @@
 package cn.cqautotest.sunnybeach.ui.fragment
 
+import androidx.fragment.app.viewModels
 import cn.cqautotest.sunnybeach.R
 import cn.cqautotest.sunnybeach.app.AppActivity
 import cn.cqautotest.sunnybeach.app.TitleBarFragment
@@ -7,6 +8,8 @@ import cn.cqautotest.sunnybeach.databinding.MyMeFragmentBinding
 import cn.cqautotest.sunnybeach.ui.activity.BrowserActivity
 import cn.cqautotest.sunnybeach.ui.activity.SettingActivity
 import cn.cqautotest.sunnybeach.util.MAKE_COMPLAINTS_URL
+import cn.cqautotest.sunnybeach.viewmodel.UserViewModel
+import com.bumptech.glide.Glide
 
 /**
  * author : A Lonely Cat
@@ -18,6 +21,7 @@ class MyMeFragment : TitleBarFragment<AppActivity>() {
 
     private var _binding: MyMeFragmentBinding? = null
     private val mBinding get() = _binding!!
+    private val mUserViewModel by viewModels<UserViewModel>()
 
     override fun getLayoutId(): Int = R.layout.my_me_fragment
 
@@ -25,7 +29,20 @@ class MyMeFragment : TitleBarFragment<AppActivity>() {
         _binding = MyMeFragmentBinding.bind(view)
     }
 
-    override fun initObserver() {}
+    override fun onFragmentResume(first: Boolean) {
+        super.onFragmentResume(first)
+        val userBasicInfo = mUserViewModel.loadUserBasicInfo()
+        val meContent = mBinding.meContent
+        Glide.with(this)
+            .load(userBasicInfo?.avatar)
+            .circleCrop()
+            .into(meContent.imageAvatar)
+        meContent.textNickName.text = userBasicInfo?.nickname
+    }
+
+    override fun initObserver() {
+
+    }
 
     override fun initEvent() {
         val meContent = mBinding.meContent
@@ -37,7 +54,9 @@ class MyMeFragment : TitleBarFragment<AppActivity>() {
         }
     }
 
-    override fun initData() {}
+    override fun initData() {
+        mUserViewModel.checkUserToken()
+    }
 
     override fun initView() {}
 
