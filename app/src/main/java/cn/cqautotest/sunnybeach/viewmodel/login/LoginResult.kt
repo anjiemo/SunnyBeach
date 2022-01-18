6253e3d1a761cf3e85ef6ea24ab8a05aaa31e561
anjiemo <2695734816@qq.com>
2022-01-18 15:27:30 +0800

    重构登录功能逻辑代码。

--- app/src/main/java/cn/cqautotest/sunnybeach/viewmodel/login/LoginResult.kt
+++
@@ -1,9 +0,0 @@
-package cn.cqautotest.sunnybeach.viewmodel.login
-
-/**
- * 身份验证结果：成功（用户详细信息）或错误消息
- */
-data class LoginResult(
-    val success: LoggedInUserView? = null,
-    val error: String? = null
-)
