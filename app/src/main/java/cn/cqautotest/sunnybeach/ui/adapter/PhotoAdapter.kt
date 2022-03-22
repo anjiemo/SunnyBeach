7ccd38f4cfb837e51f2405af83f9ed98ab989827
anjiemo <2695734816@qq.com>
2022-03-22 17:10:21 +0800

    优化图片列表适配器代码。

--- app/src/main/java/cn/cqautotest/sunnybeach/ui/adapter/PhotoAdapter.kt
+++ app/src/main/java/cn/cqautotest/sunnybeach/ui/adapter/PhotoAdapter.kt
@@ -1,6 +1,5 @@
 package cn.cqautotest.sunnybeach.ui.adapter
 
-import android.graphics.drawable.Drawable
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
@@ -11,13 +10,10 @@ import cn.cqautotest.sunnybeach.util.dp
 import cn.cqautotest.sunnybeach.widget.RoundRelativeLayout
 import com.blankj.utilcode.util.ScreenUtils
 import com.bumptech.glide.Glide
-import com.bumptech.glide.request.target.ImageViewTarget
-import com.bumptech.glide.request.transition.Transition
 import com.chad.library.adapter.base.BaseQuickAdapter
 import com.chad.library.adapter.base.module.LoadMoreModule
 import com.chad.library.adapter.base.viewholder.BaseViewHolder
 import com.hjq.widget.layout.RatioFrameLayout
-import timber.log.Timber
 
 /**
  * author : A Lonely Cat
@@ -50,59 +46,26 @@ class PhotoAdapter(val fillBox: Boolean = false) :
             }
             val photoIv = getView<ImageView>(R.id.photoIv)
             val roundLayout = getView<RoundRelativeLayout>(R.id.round_layout)
-            val loadingView = getView<View>(R.id.loading_lav)
-            showLoading(loadingView, fillBox)
-            if (fillBox.not()) {
-                roundLayout.setRadius(8.dp.toFloat())
-                // 加载非全屏的图片列表
-                Timber.d("width is ${photoIv.width} height is ${photoIv.height}")
-                Glide.with(itemView)
-                    .load(item.thumb)
-                    .placeholder(R.mipmap.ic_bg)
-                    .override(photoIv.width, photoIv.height)
-                    .into(photoIv)
-            } else {
-                roundLayout.setRadius(0f)
+            roundLayout.setRadius((if (fillBox) 0 else 8).dp.toFloat())
+            // 设置比例布局全屏
+            if (fillBox) {
                 // 设置比例布局全屏
                 val ratioFrameLayout = getView<RatioFrameLayout>(R.id.ratio_frame_layout)
                 ratioFrameLayout.setSizeRatio(
                     ScreenUtils.getScreenWidth().toFloat(),
                     ScreenUtils.getScreenHeight().toFloat()
                 )
-                // 加载全屏的图片
-                Glide.with(itemView)
-                    .load(item.preview)
-                    .placeholder(R.mipmap.ic_bg)
-                    .into(object : ImageViewTarget<Drawable>(photoIv) {
-
-                        override fun onLoadStarted(placeholder: Drawable?) {
-                            super.onLoadStarted(placeholder)
-                            showLoading(loadingView, true)
-                        }
-
-                        override fun onLoadFailed(errorDrawable: Drawable?) {
-                            super.onLoadFailed(errorDrawable)
-                            showLoading(loadingView, false)
-                        }
-
-                        override fun onLoadCleared(placeholder: Drawable?) {
-                            super.onLoadCleared(placeholder)
-                            showLoading(loadingView, false)
-                        }
-
-                        override fun onResourceReady(
-                            resource: Drawable,
-                            transition: Transition<in Drawable>?
-                        ) {
-                            super.onResourceReady(resource, transition)
-                            showLoading(loadingView, false)
-                        }
-
-                        override fun setResource(resource: Drawable?) {
-                            getView().setImageDrawable(resource)
-                        }
-                    })
+            } else {
+                itemView.layoutParams.width = ViewGroup.LayoutParams.MATCH_PARENT
+                itemView.layoutParams.height = ViewGroup.LayoutParams.WRAP_CONTENT
             }
+            // 加载全屏的图片
+            Glide.with(itemView)
+                .load(item.preview)
+                .placeholder(R.mipmap.ic_bg).run {
+                    if (fillBox) this else override(photoIv.width, photoIv.height)
+                }
+                .into(photoIv)
             itemView.setOnClickListener {
                 mItemClickListener(item, bindingAdapterPosition)
             }
