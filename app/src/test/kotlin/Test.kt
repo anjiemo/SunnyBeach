8bab6c224f6af6a21df6f3693168ef3330a90b92
anjiemo <2695734816@qq.com>
2022-04-18 20:26:48 +0800

    根据 Api 类型使用不同的 Retrofit 实例创建请求 Api 实例。

---
+++ app/src/test/kotlin/Test.kt
@@ -0,0 +1,11 @@
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+import org.junit.Test
+
+class Test {
+
+    @Test
+    fun test(): Unit = runBlocking(Dispatchers.IO) {
+
+    }
+}
