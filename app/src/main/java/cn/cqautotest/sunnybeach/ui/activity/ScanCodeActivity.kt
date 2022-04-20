ec2a7621074238c3c21ab1ed6effaf9fe3367b51
anjiemo <2695734816@qq.com>
2022-04-21 00:16:21 +0800

    二维码角点中心计算实现。

--- app/src/main/java/cn/cqautotest/sunnybeach/ui/activity/ScanCodeActivity.kt
+++ app/src/main/java/cn/cqautotest/sunnybeach/ui/activity/ScanCodeActivity.kt
@@ -3,15 +3,21 @@ package cn.cqautotest.sunnybeach.ui.activity
 import android.app.Activity
 import android.content.Intent
 import android.graphics.Bitmap
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore.Images
+import android.view.View
+import android.widget.FrameLayout
+import androidx.core.view.forEach
 import androidx.lifecycle.lifecycleScope
 import by.kirich1409.viewbindingdelegate.viewBinding
 import cn.cqautotest.sunnybeach.R
 import cn.cqautotest.sunnybeach.app.AppActivity
 import cn.cqautotest.sunnybeach.databinding.ScankitZxlCaptureNewBinding
 import cn.cqautotest.sunnybeach.util.DownloadHelper
+import cn.cqautotest.sunnybeach.util.dp
 import cn.cqautotest.sunnybeach.util.setFixOnClickListener
 import com.huawei.hms.hmsscankit.RemoteView
 import com.huawei.hms.hmsscankit.ScanUtil
@@ -19,6 +25,7 @@ import com.huawei.hms.ml.scan.HmsScan
 import com.huawei.hms.ml.scan.HmsScanAnalyzerOptions
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import timber.log.Timber
 
 /**
  * author : A Lonely Cat
@@ -60,6 +67,7 @@ class ScanCodeActivity : AppActivity() {
         }
         mBinding.imgBtn.setFixOnClickListener {
             val intent = Intent(Intent.ACTION_PICK, Images.Media.EXTERNAL_CONTENT_URI)
+            intent.setDataAndType(Images.Media.EXTERNAL_CONTENT_URI, "image/*")
             startActivityForResult(intent) { resultCode, data ->
                 if (resultCode == Activity.RESULT_OK && data != null) {
                     val uri = data.data ?: run {
@@ -91,9 +99,44 @@ class ScanCodeActivity : AppActivity() {
     private fun setResultAndFinish(hmsScans: Array<out HmsScan>?) {
         val intent = Intent()
         val hmsScan = if (hmsScans.isNullOrEmpty()) null else hmsScans[0]
+        hmsScan?.getCornerPoints()?.let { pointArray ->
+            val flContainer = mBinding.flContainer
+            flContainer.forEach {
+                val view = flContainer.findViewWithTag<View>("point")
+                if (view != null) {
+                    flContainer.removeView(view)
+                }
+            }
+            var minX = pointArray[0].x
+            var maxX = pointArray[0].x
+            var minY = pointArray[0].y
+            var maxY = pointArray[0].y
+            pointArray.onEach { point ->
+                minX = minX.coerceAtMost(point.x)
+                maxX = maxX.coerceAtLeast(point.x)
+                minY = minY.coerceAtMost(point.y)
+                maxY = maxY.coerceAtLeast(point.y)
+                Timber.d("setResultAndFinish：===> $point")
+            }
+            val pointView by lazy {
+                View(this).apply {
+                    tag = "point"
+                    background = ColorDrawable(Color.RED)
+                }.also {
+                    it.layoutParams = FrameLayout.LayoutParams(7.dp, 7.dp)
+                }
+            }
+            val x = (minX + maxX) / 2 + 24.dp
+            val y = (minY + maxY) / 2 + 46.dp
+            pointView.x = x.toFloat()
+            pointView.y = y.toFloat()
+            mBinding.flContainer.addView(pointView)
+            Timber.d("setResultAndFinish：======== 分割线 =========>")
+            // remoteView.onPause()
+        }
         intent.putExtra(ScanUtil.RESULT, hmsScan)
         setResult(RESULT_OK, intent)
-        finish()
+        // finish()
     }
 
     override fun onStart() {
