0d20e162467ee03fa70000d0bef08681b172afd2
Administrator <123@qq.com>
2022-04-21 13:59:11 +0800

    将需要跳转的扫码界面替换为自定义的扫码界面，以修复弹出选择图片应用对话框导致的无法正常扫码的问题。

--- app/src/main/java/cn/cqautotest/sunnybeach/ui/activity/ScanCodeActivity.kt
+++ app/src/main/java/cn/cqautotest/sunnybeach/ui/activity/ScanCodeActivity.kt
@@ -2,30 +2,20 @@ package cn.cqautotest.sunnybeach.ui.activity
 
 import android.app.Activity
 import android.content.Intent
-import android.graphics.Bitmap
-import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
 import android.net.Uri
 import android.os.Bundle
-import android.provider.MediaStore.Images
+import android.provider.MediaStore
 import android.view.View
-import android.widget.FrameLayout
-import androidx.core.view.forEach
-import androidx.lifecycle.lifecycleScope
-import by.kirich1409.viewbindingdelegate.viewBinding
 import cn.cqautotest.sunnybeach.R
-import cn.cqautotest.sunnybeach.app.AppActivity
-import cn.cqautotest.sunnybeach.databinding.ScankitZxlCaptureNewBinding
-import cn.cqautotest.sunnybeach.util.DownloadHelper
-import cn.cqautotest.sunnybeach.util.dp
+import cn.cqautotest.sunnybeach.action.Init
+import cn.cqautotest.sunnybeach.http.glide.GlideApp
+import cn.cqautotest.sunnybeach.manager.ThreadPoolManager
 import cn.cqautotest.sunnybeach.util.setFixOnClickListener
-import com.huawei.hms.hmsscankit.RemoteView
+import com.dylanc.longan.toast
+import com.huawei.hms.hmsscankit.ScanKitActivity
 import com.huawei.hms.hmsscankit.ScanUtil
 import com.huawei.hms.ml.scan.HmsScan
 import com.huawei.hms.ml.scan.HmsScanAnalyzerOptions
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
-import timber.log.Timber
 
 /**
  * author : A Lonely Cat
@@ -33,131 +23,63 @@ import timber.log.Timber
  * time   : 2022/04/20
  * desc   : 自定义的扫码界面，暂支持扫描二维码
  */
-class ScanCodeActivity : AppActivity() {
-
-    private val mBinding by viewBinding<ScankitZxlCaptureNewBinding>()
-    private var instanceState: Bundle? = null
-    private lateinit var remoteView: RemoteView
-
-    override fun getLayoutId(): Int = R.layout.scankit_zxl_capture_new
+class ScanCodeActivity : ScanKitActivity(), Init {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        instanceState = savedInstanceState
-    }
-
-    override fun initView() {
-        remoteView = RemoteView.Builder()
-            .setContext(this)
-            .build()
-        remoteView.setOnResultCallback {
-            setResultAndFinish(it)
-        }
-        remoteView.onCreate(instanceState)
-        mBinding.flContainer.addView(remoteView)
-    }
-
-    override fun initData() {
-
+        initEvent()
     }
 
     override fun initEvent() {
-        mBinding.backImgIn.setFixOnClickListener {
-            finish()
+        findViewById<View>(R.id.img_btn).setFixOnClickListener {
+            selectImage()
         }
-        mBinding.imgBtn.setFixOnClickListener {
-            val intent = Intent(Intent.ACTION_PICK, Images.Media.EXTERNAL_CONTENT_URI)
-            intent.setDataAndType(Images.Media.EXTERNAL_CONTENT_URI, "image/*")
-            startActivityForResult(intent) { resultCode, data ->
-                if (resultCode == Activity.RESULT_OK && data != null) {
-                    val uri = data.data ?: run {
-                        toast("未选择图片")
-                        return@startActivityForResult
-                    }
-                    parseScanResult(uri)
-                } else {
-                    setResultAndFinish(null)
-                }
+    }
+
+    private fun selectImage() {
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        intent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*")
+        startActivityForResult(intent, REQUEST_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK && data != null) {
+            val uri = data.data ?: run {
+                toast("未选择图片")
+                return
             }
+            parseScanResult(uri)
+        } else {
+            setResultAndFinish(null)
         }
     }
 
     private fun parseScanResult(uri: Uri) {
-        lifecycleScope.launchWhenCreated {
-            withContext(Dispatchers.IO) {
-                val bitmap = DownloadHelper.ofType<Bitmap>(this@ScanCodeActivity, uri)
-                // “QRCODE_SCAN_TYPE”和“DATAMATRIX_SCAN_TYPE”表示只扫描QR和Data Matrix的码
-                val options = HmsScanAnalyzerOptions.Creator()
-                    .setHmsScanTypes(HmsScan.QRCODE_SCAN_TYPE)
-                    .create()
-                val hmsScans = ScanUtil.decodeWithBitmap(this@ScanCodeActivity, bitmap, options)
-                setResultAndFinish(hmsScans)
-            }
+        ThreadPoolManager.getInstance().execute {
+            val bitmap = GlideApp.with(this)
+                .asBitmap()
+                .load(uri)
+                .submit()
+                .get()
+            // “QRCODE_SCAN_TYPE”和“DATAMATRIX_SCAN_TYPE”表示只扫描QR和Data Matrix的码
+            val options = HmsScanAnalyzerOptions.Creator()
+                .setHmsScanTypes(HmsScan.QRCODE_SCAN_TYPE)
+                .create()
+            val hmsScans = ScanUtil.decodeWithBitmap(this@ScanCodeActivity, bitmap, options)
+            setResultAndFinish(hmsScans)
         }
     }
 
     private fun setResultAndFinish(hmsScans: Array<out HmsScan>?) {
         val intent = Intent()
         val hmsScan = if (hmsScans.isNullOrEmpty()) null else hmsScans[0]
-        hmsScan?.getCornerPoints()?.let { pointArray ->
-            val flContainer = mBinding.flContainer
-            flContainer.forEach {
-                val view = flContainer.findViewWithTag<View>("point")
-                if (view != null) {
-                    flContainer.removeView(view)
-                }
-            }
-            var minX = pointArray[0].x
-            var maxX = pointArray[0].x
-            var minY = pointArray[0].y
-            var maxY = pointArray[0].y
-            pointArray.onEach { point ->
-                minX = minX.coerceAtMost(point.x)
-                maxX = maxX.coerceAtLeast(point.x)
-                minY = minY.coerceAtMost(point.y)
-                maxY = maxY.coerceAtLeast(point.y)
-                Timber.d("setResultAndFinish：===> $point")
-            }
-            val pointView by lazy {
-                View(this).apply {
-                    tag = "point"
-                    background = ColorDrawable(Color.RED)
-                }.also {
-                    it.layoutParams = FrameLayout.LayoutParams(7.dp, 7.dp)
-                }
-            }
-            val x = (minX + maxX) / 2 + 24.dp
-            val y = (minY + maxY) / 2 + 46.dp
-            pointView.x = x.toFloat()
-            pointView.y = y.toFloat()
-            mBinding.flContainer.addView(pointView)
-            Timber.d("setResultAndFinish：======== 分割线 =========>")
-            // remoteView.onPause()
-        }
         intent.putExtra(ScanUtil.RESULT, hmsScan)
         setResult(RESULT_OK, intent)
-        // finish()
+        finish()
     }
 
-    override fun onStart() {
-        super.onStart()
-        remoteView.onStart()
+    companion object {
+        private const val REQUEST_CODE = 0x123456
     }
-
-    override fun onPause() {
-        super.onPause()
-        remoteView.onPause()
-    }
-
-    override fun onResume() {
-        super.onResume()
-        remoteView.onResume()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        remoteView.onDestroy()
-    }
-
-    override fun isStatusBarEnabled(): Boolean = false
 }
--- app/src/main/java/cn/cqautotest/sunnybeach/util/Any.kt
+++ app/src/main/java/cn/cqautotest/sunnybeach/util/Any.kt
@@ -1,10 +1,6 @@
 package cn.cqautotest.sunnybeach.util
 
-import android.app.Activity
-import android.content.Intent
-import cn.cqautotest.sunnybeach.ui.activity.ScanCodeActivity
 import com.hjq.gson.factory.GsonFactory
-import com.huawei.hms.ml.scan.HmsScanAnalyzerOptions
 
 val Any.TAG
     get() = javaClass.simpleName
@@ -13,8 +9,3 @@ fun Any.toJson() = GsonFactory.getSingletonGson().toJson(this)
 
 inline fun <reified T> Any.fromJson(json: String?) =
     GsonFactory.getSingletonGson().fromJson(json, T::class.java)
-
-fun startScan(activity: Activity, requestCode: Int, options: HmsScanAnalyzerOptions) {
-    val intent = Intent(activity, ScanCodeActivity::class.java)
-    activity.startActivityForResult(intent, requestCode)
-}
