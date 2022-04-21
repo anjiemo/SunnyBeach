89d85a9ace320aff59bf414dfb85048de6903709
anjiemo <2695734816@qq.com>
2022-04-21 23:55:04 +0800

    新增学院课程相关 api 。

---
+++ app/src/main/java/cn/cqautotest/sunnybeach/http/api/sob/CourseApi.kt
@@ -0,0 +1,29 @@
+package cn.cqautotest.sunnybeach.http.api.sob
+
+import cn.cqautotest.sunnybeach.model.ApiResponse
+import cn.cqautotest.sunnybeach.model.course.Course
+import cn.cqautotest.sunnybeach.model.course.CourseChapter
+import cn.cqautotest.sunnybeach.model.course.CourseDetail
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface CourseApi : ISobApi {
+
+    /**
+     * 获取课程列表
+     */
+    @GET("ct/edu/course/list/{page}")
+    suspend fun getCourseList(@Path("page") page: Int): ApiResponse<Course>
+
+    /**
+     * 获取课程详情
+     */
+    @GET("ct/edu/course/{courseId}")
+    suspend fun getCourseDetail(@Path("courseId") courseId: String): ApiResponse<CourseDetail>
+
+    /**
+     * 获取课程章节内容
+     */
+    @GET("ct/edu/course/chapter/{courseId}")
+    suspend fun getCourseChapter(@Path("courseId") courseId: String): ApiResponse<CourseChapter>
+}
