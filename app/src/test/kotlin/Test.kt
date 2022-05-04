254e7470de348eee6f450bf435f1dde347d84927
anjiemo <2695734816@qq.com>
2022-05-04 23:36:57 +0800

    修改README文件（修改项目截图）。

--- app/src/test/kotlin/Test.kt
+++ app/src/test/kotlin/Test.kt
@@ -1,11 +1,40 @@
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
 import org.junit.Test
+import java.io.File
 
 class Test {
 
     @Test
     fun test(): Unit = runBlocking(Dispatchers.IO) {
 
+    }
+
+    @Test
+    fun listFiles() {
+        val file = File("/Users/anjiemo/Pictures/阳光沙滩")
+        val fileList = file.listFiles() ?: run {
+            println("listFiles：===> 路径无效...")
+            return
+        }
+        println("listFiles：===> fileList size is ${fileList.size}")
+        val str = buildString {
+            append("\n")
+            append("========= 分割线 =========")
+            append("\n")
+            fileList.asSequence()
+                .filterNot { it.name.contains(".DS_Store") }
+                .sortedBy { it.lastModified() }
+                .forEach {
+                    append("\n")
+                    append("![](")
+                    append("./picture/sunnybeach/")
+                    append(it.name)
+                    append(")")
+                }
+            append("\n")
+            append("========= 分割线 =========")
+        }
+        println("listFiles：===> $str")
     }
 }
