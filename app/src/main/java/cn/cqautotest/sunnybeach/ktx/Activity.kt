f3e18d1021ea513582831ab79f7a41cd8b94ea93
anjiemo <2695734816@qq.com>
2022-05-13 00:25:51 +0800

    禁止跟随系统字体大小缩放（防止界面错乱）。

--- app/src/main/java/cn/cqautotest/sunnybeach/ktx/Activity.kt
+++ app/src/main/java/cn/cqautotest/sunnybeach/ktx/Activity.kt
@@ -3,6 +3,8 @@
 package cn.cqautotest.sunnybeach.ktx
 
 import android.app.Activity
+import android.content.res.Configuration
+import android.content.res.Resources
 import com.blankj.utilcode.util.KeyboardUtils
 
 fun Activity.hideKeyboard() {
@@ -11,3 +13,14 @@ fun Activity.hideKeyboard() {
 
 val Activity.decorView
     get() = window.decorView
+
+fun Activity.resetConfiguration() = resources.resetConfiguration()
+
+/**
+ * 重置界面配置，解决系统字体过大或者过小时导致的界面错乱问题
+ */
+fun Resources.resetConfiguration() {
+    val config = Configuration()
+    config.setToDefaults()
+    updateConfiguration(config, displayMetrics)
+}
