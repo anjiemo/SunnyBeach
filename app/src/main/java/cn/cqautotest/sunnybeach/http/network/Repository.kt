29fc1daddc1ca12b6c2e1e0889f9a4254524d963
anjiemo <2695734816@qq.com>
2022-05-18 00:03:22 +0800

    简化 Repository 中部分函数的调用方式。

--- app/src/main/java/cn/cqautotest/sunnybeach/http/network/Repository.kt
+++ app/src/main/java/cn/cqautotest/sunnybeach/http/network/Repository.kt
@@ -1,7 +1,6 @@
 package cn.cqautotest.sunnybeach.http.network
 
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.LiveDataScope
 import androidx.lifecycle.liveData
 import cn.cqautotest.sunnybeach.db.dao.PlaceDao
 import cn.cqautotest.sunnybeach.execption.NotLoginException
@@ -13,6 +12,7 @@ import cn.cqautotest.sunnybeach.model.*
 import cn.cqautotest.sunnybeach.model.wallpaper.WallpaperBean
 import cn.cqautotest.sunnybeach.model.weather.Place
 import cn.cqautotest.sunnybeach.model.weather.Weather
+import com.huawei.hms.scankit.p.T
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
 import kotlinx.coroutines.coroutineScope
@@ -21,6 +21,7 @@ import okhttp3.MultipartBody
 import okhttp3.RequestBody
 import timber.log.Timber
 import java.io.File
+import kotlin.coroutines.CoroutineContext
 
 /**
  *    author : A Lonely Cat
@@ -34,53 +35,29 @@ object Repository {
 
     private val cachePhotoIdList = arrayListOf<WallpaperBean.Res.Vertical>()
 
-    fun getCoursePlayAuth(videoId: String) = liveData(Dispatchers.IO) {
-        launchAndGetData { CourseNetwork.getCoursePlayAuth(videoId) }
-    }
+    fun getCoursePlayAuth(videoId: String) = launchAndGetData { CourseNetwork.getCoursePlayAuth(videoId) }
 
-    fun modifyPasswordBySms(smsCode: String, user: User) = liveData(Dispatchers.IO) {
-        launchAndGetMsg { UserNetwork.modifyPasswordBySms(smsCode, user) }
-    }
+    fun modifyPasswordBySms(smsCode: String, user: User) = launchAndGetMsg { UserNetwork.modifyPasswordBySms(smsCode, user) }
 
-    fun modifyPasswordByOldPwd(modifyPwd: ModifyPwd) = liveData(Dispatchers.IO) {
-        launchAndGetMsg { UserNetwork.modifyPasswordByOldPwd(modifyPwd) }
-    }
+    fun modifyPasswordByOldPwd(modifyPwd: ModifyPwd) = launchAndGetMsg { UserNetwork.modifyPasswordByOldPwd(modifyPwd) }
 
-    fun checkSmsCode(phoneNumber: String, smsCode: String) = liveData(Dispatchers.IO) {
-        launchAndGetMsg { UserNetwork.checkSmsCode(phoneNumber, smsCode) }
-    }
+    fun checkSmsCode(phoneNumber: String, smsCode: String) = launchAndGetMsg { UserNetwork.checkSmsCode(phoneNumber, smsCode) }
 
-    fun sendForgetSmsVerifyCode(smsInfo: SmsInfo) = liveData(Dispatchers.IO) {
-        launchAndGetMsg { UserNetwork.sendForgetSmsVerifyCode(smsInfo) }
-    }
+    fun sendForgetSmsVerifyCode(smsInfo: SmsInfo) = launchAndGetMsg { UserNetwork.sendForgetSmsVerifyCode(smsInfo) }
 
-    fun registerAccount(smsCode: String, user: User) = liveData(Dispatchers.IO) {
-        launchAndGetMsg { UserNetwork.registerAccount(smsCode, user) }
-    }
+    fun registerAccount(smsCode: String, user: User) = launchAndGetMsg { UserNetwork.registerAccount(smsCode, user) }
 
-    fun sendRegisterSmsVerifyCode(smsInfo: SmsInfo) = liveData(Dispatchers.IO) {
-        launchAndGetMsg { UserNetwork.sendRegisterSmsVerifyCode(smsInfo) }
-    }
+    fun sendRegisterSmsVerifyCode(smsInfo: SmsInfo) = launchAndGetMsg { UserNetwork.sendRegisterSmsVerifyCode(smsInfo) }
 
-    fun getSobIEDetailList(userId: String, page: Int) = liveData(Dispatchers.IO) {
-        launchAndGetData { UserNetwork.getSobIEDetailList(userId, page) }
-    }
+    fun getSobIEDetailList(userId: String, page: Int) = launchAndGetData { UserNetwork.getSobIEDetailList(userId, page) }
 
-    fun getVipUserList() = liveData(Dispatchers.IO) {
-        launchAndGetData { UserNetwork.getVipUserList() }
-    }
+    fun getVipUserList() = launchAndGetData { UserNetwork.getVipUserList() }
 
-    fun getAchievement() = liveData(Dispatchers.IO) {
-        launchAndGetData { UserNetwork.getAchievement() }
-    }
+    fun getAchievement() = launchAndGetData { UserNetwork.getAchievement() }
 
-    fun queryTotalSobCount() = liveData(Dispatchers.IO) {
-        launchAndGetData { UserNetwork.queryTotalSobCount() }
-    }
+    fun queryTotalSobCount() = launchAndGetData { UserNetwork.queryTotalSobCount() }
 
-    fun queryUserInfo() = liveData(Dispatchers.IO) {
-        launchAndGetData { UserNetwork.queryUserInfo() }
-    }
+    fun queryUserInfo() = launchAndGetData { UserNetwork.queryUserInfo() }
 
     fun getAllowance() = liveData(Dispatchers.IO) {
         val result = try {
@@ -122,25 +99,15 @@ object Repository {
         emit(result)
     }
 
-    fun unfollowUser(userId: String) = liveData(Dispatchers.IO) {
-        launchAndGetData { UserNetwork.unfollowUser(userId) }
-    }
+    fun unfollowUser(userId: String) = launchAndGetData { UserNetwork.unfollowUser(userId) }
 
-    fun followUser(userId: String) = liveData(Dispatchers.IO) {
-        launchAndGetData { UserNetwork.followUser(userId) }
-    }
+    fun followUser(userId: String) = launchAndGetData { UserNetwork.followUser(userId) }
 
-    fun followState(userId: String) = liveData(Dispatchers.IO) {
-        launchAndGetData { UserNetwork.followState(userId) }
-    }
+    fun followState(userId: String) = launchAndGetData { UserNetwork.followState(userId) }
 
-    fun getUserInfo(userId: String) = liveData(Dispatchers.IO) {
-        launchAndGetData { UserNetwork.getUserInfo(userId) }
-    }
+    fun getUserInfo(userId: String) = launchAndGetData { UserNetwork.getUserInfo(userId) }
 
-    fun getAchievement(userId: String) = liveData(Dispatchers.IO) {
-        launchAndGetData { UserNetwork.getAchievement(userId) }
-    }
+    fun getAchievement(userId: String) = launchAndGetData { UserNetwork.getAchievement(userId) }
 
     fun logout() = liveData(Dispatchers.IO) {
         val result = try {
@@ -202,13 +169,9 @@ object Repository {
         emit(result)
     }
 
-    fun checkAppUpdate(): LiveData<Result<AppUpdateInfo>> = liveData(Dispatchers.IO) {
-        launchAndGetData { AppNetwork.checkAppUpdate() }
-    }
+    fun checkAppUpdate(): LiveData<Result<AppUpdateInfo>> = launchAndGetData { AppNetwork.checkAppUpdate() }
 
-    fun getMourningCalendar(): LiveData<Result<List<MourningCalendar>>> = liveData(Dispatchers.IO) {
-        launchAndGetData { AppNetwork.getMourningCalendar() }
-    }
+    fun getMourningCalendar(): LiveData<Result<List<MourningCalendar>>> = launchAndGetData { AppNetwork.getMourningCalendar() }
 
     fun setPhotoIdList(photoIdList: List<WallpaperBean.Res.Vertical>) {
         if (photoIdList !== cachePhotoIdList) {
@@ -229,9 +192,7 @@ object Repository {
 
     fun getPhotoList() = cachePhotoIdList.toList()
 
-    fun getFishCommendListById(momentId: String, page: Int) = liveData(Dispatchers.IO) {
-        launchAndGetData { FishNetwork.getFishCommendListById(momentId, page) }
-    }
+    fun getFishCommendListById(momentId: String, page: Int) = launchAndGetData { FishNetwork.getFishCommendListById(momentId, page) }
 
     suspend fun uploadFishImage(imageFile: File): String? {
         return try {
@@ -251,13 +212,9 @@ object Repository {
         }
     }
 
-    fun putFish(moment: Map<String, Any?>) = liveData(Dispatchers.IO) {
-        launchAndGetData { FishNetwork.putFish(moment) }
-    }
+    fun putFish(moment: Map<String, Any?>) = launchAndGetData { FishNetwork.putFish(moment) }
 
-    fun loadFishDetailById(momentId: String) = liveData(Dispatchers.IO) {
-        launchAndGetData { FishNetwork.loadFishDetailById(momentId) }
-    }
+    fun loadFishDetailById(momentId: String) = launchAndGetData { FishNetwork.loadFishDetailById(momentId) }
 
     fun loadWallpaperBannerList() = liveData(Dispatchers.IO) {
         val result = try {
@@ -274,64 +231,42 @@ object Repository {
         emit(result)
     }
 
-    fun postComment(momentComment: Map<String, Any?>, isReply: Boolean) = liveData(Dispatchers.IO) {
-        launchAndGetData {
-            if (isReply) FishNetwork.replyComment(momentComment)
-            else FishNetwork.postComment(momentComment)
-        }
+    fun postComment(momentComment: Map<String, Any?>, isReply: Boolean) = launchAndGetData {
+        if (isReply) FishNetwork.replyComment(momentComment)
+        else FishNetwork.postComment(momentComment)
     }
 
-    fun dynamicLikes(momentId: String) = liveData(Dispatchers.IO) {
-        launchAndGetMsg { FishNetwork.dynamicLikes(momentId) }
-    }
+    fun dynamicLikes(momentId: String) = launchAndGetMsg { FishNetwork.dynamicLikes(momentId) }
 
-    fun loadTopicList() = liveData(Dispatchers.IO) {
-        launchAndGetData { FishNetwork.loadTopicList() }
-    }
+    fun loadTopicList() = launchAndGetData { FishNetwork.loadTopicList() }
 
-    fun getRichList() = liveData(Dispatchers.IO) {
-        launchAndGetData { UserNetwork.getRichList() }
-    }
+    fun getRichList() = launchAndGetData { UserNetwork.getRichList() }
 
-    fun readQaMsg(msgId: String) = liveData(Dispatchers.IO) {
-        launchAndGetMsg { MsgNetwork.readQaMsg(msgId) }
-    }
+    fun readQaMsg(msgId: String) = launchAndGetMsg { MsgNetwork.readQaMsg(msgId) }
 
-    fun readAtMeMsg(msgId: String) = liveData(Dispatchers.IO) {
-        launchAndGetMsg { MsgNetwork.readAtMeMsg(msgId) }
-    }
+    fun readAtMeMsg(msgId: String) = launchAndGetMsg { MsgNetwork.readAtMeMsg(msgId) }
 
-    fun readMomentMsg(msgId: String) = liveData(Dispatchers.IO) {
-        launchAndGetMsg { MsgNetwork.readMomentMsg(msgId) }
-    }
+    fun readMomentMsg(msgId: String) = launchAndGetMsg { MsgNetwork.readMomentMsg(msgId) }
 
     /**
      * 更新文章消息为：已回复
      */
-    fun replyArticleMsg(msgId: String) = liveData(Dispatchers.IO) {
-        launchAndGetMsg { MsgNetwork.readArticleMsg(msgId, 2) }
-    }
+    fun replyArticleMsg(msgId: String) = launchAndGetMsg { MsgNetwork.readArticleMsg(msgId, 2) }
 
     /**
      * 更新文章消息为：已读
      */
-    fun readArticleMsg(msgId: String) = liveData(Dispatchers.IO) {
-        launchAndGetMsg { MsgNetwork.readArticleMsg(msgId, 1) }
-    }
+    fun readArticleMsg(msgId: String) = launchAndGetMsg { MsgNetwork.readArticleMsg(msgId, 1) }
 
     /**
      * 一键已读所有消息
      */
-    fun readAllMsg() = liveData(Dispatchers.IO) {
-        launchAndGetMsg { MsgNetwork.readAllMsg() }
-    }
+    fun readAllMsg() = launchAndGetMsg { MsgNetwork.readAllMsg() }
 
     /**
      * 获取未读消息数量
      */
-    fun getUnReadMsgCount() = liveData(Dispatchers.IO) {
-        launchAndGetData { MsgNetwork.getUnReadMsgCount() }
-    }
+    fun getUnReadMsgCount() = launchAndGetData { MsgNetwork.getUnReadMsgCount() }
 
     fun searchPlaces(query: String) = liveData(Dispatchers.IO) {
         Timber.d(query)
@@ -401,32 +336,33 @@ object Repository {
     /**
      * 启动并获取数据
      */
-    private suspend inline fun <reified T> LiveDataScope<Result<T>>.launchAndGetData(crossinline action: suspend () -> ApiResponse<T>) {
-        launchAndGet(action = action, onSuccess = { it.getData() })
-    }
+    private inline fun <reified T> launchAndGetData(
+        context: CoroutineContext = Dispatchers.IO,
+        crossinline action: suspend () -> ApiResponse<T>
+    ) =
+        launchAndGet(context = context, action = action, onSuccess = { it.getData() })
 
     /**
      * 启动并获取消息
      */
-    private suspend inline fun LiveDataScope<Result<Any>>.launchAndGetMsg(crossinline action: suspend () -> ApiResponse<Any>) {
-        launchAndGet(action = action, onSuccess = { it.getMessage() })
-    }
+    private inline fun launchAndGetMsg(context: CoroutineContext = Dispatchers.IO, crossinline action: suspend () -> ApiResponse<Any>) =
+        launchAndGet(context = context, action = action, onSuccess = { it.getMessage() })
 
     /**
      * 启动并获取
-     * 成功时返回：Result.success()
-     * 失败时返回：Result.failure()
+     * 返回一个
      */
-    private suspend inline fun <reified T> LiveDataScope<Result<T>>.launchAndGet(
+    private inline fun <reified T> launchAndGet(
+        context: CoroutineContext = Dispatchers.IO,
         // 需要调用的接口
         crossinline action: suspend () -> ApiResponse<T>,
         // 请求成功时的回调
         crossinline onSuccess: (ApiResponse<T>) -> T
-    ) {
+    ) = liveData(context = context) {
         val result = try {
             coroutineScope {
                 val result = action.invoke()
-                Timber.d("result is ${result.toJson()}")
+                Timber.d("launchAndGet：===> result is ${result.toJson()}")
                 if (result.isSuccess()) Result.success(onSuccess.invoke(result))
                 else when (result.getCode()) {
                     NOT_LOGIN_CODE -> Result.failure(NotLoginException(result.getMessage()))
