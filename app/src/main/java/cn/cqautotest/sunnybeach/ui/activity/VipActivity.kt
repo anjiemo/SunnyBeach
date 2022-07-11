98bd2ea055147a99a8b224e842415b65bac65b5d
anjiemo <2695734816@qq.com>
2022-07-12 00:39:19 +0800

    新增阳光沙滩Vip开通渠道。

--- app/src/main/java/cn/cqautotest/sunnybeach/ui/activity/VipActivity.kt
+++ app/src/main/java/cn/cqautotest/sunnybeach/ui/activity/VipActivity.kt
@@ -1,13 +1,18 @@
 package cn.cqautotest.sunnybeach.ui.activity
 
+import android.content.Intent
+import android.net.Uri
 import by.kirich1409.viewbindingdelegate.viewBinding
 import cn.cqautotest.sunnybeach.R
 import cn.cqautotest.sunnybeach.app.AppActivity
 import cn.cqautotest.sunnybeach.app.AppFragment
 import cn.cqautotest.sunnybeach.databinding.VipActivityBinding
 import cn.cqautotest.sunnybeach.ktx.clearTooltipText
+import cn.cqautotest.sunnybeach.ui.dialog.MessageDialog
 import cn.cqautotest.sunnybeach.ui.fragment.VipIntroFragment
 import cn.cqautotest.sunnybeach.ui.fragment.VipListFragment
+import com.blankj.utilcode.util.AppUtils
+import com.hjq.bar.TitleBar
 import com.hjq.base.FragmentPagerAdapter
 
 /**
@@ -34,6 +39,31 @@ class VipActivity : AppActivity() {
         mBinding.viewPager.adapter = mFragmentAdapter
         mBinding.tabLayout.clearTooltipText()
     }
+
+    override fun onRightClick(titleBar: TitleBar) {
+        MessageDialog.Builder(this)
+            .setTitle("系统消息")
+            .setMessage("即将打开淘宝店铺，请确认")
+            .setConfirm("立即打开")
+            .setCancel("我再想想")
+            .setListener { tryOpenTaobaoApp() }
+            .show()
+    }
+
+    private fun tryOpenTaobaoApp() {
+        val installTaobaoApp = AppUtils.isAppInstalled("com.taobao.taobao")
+        takeIf { installTaobaoApp }?.let { openTaobaoApp() } ?: toast("您的设备似乎没有安装淘宝APP哦~")
+    }
+
+    private fun openTaobaoApp() {
+        val uri = Uri.parse("https://item.taobao.com/item.htm?id=673527744707&mt=")
+        startActivity(Intent(Intent.ACTION_VIEW, uri).apply {
+            setClassName("com.taobao.taobao", "com.taobao.tao.detail.activity.DetailActivity")
+            addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        })
+    }
+
+    override fun isStatusBarDarkFont() = true
 
     override fun onDestroy() {
         super.onDestroy()
