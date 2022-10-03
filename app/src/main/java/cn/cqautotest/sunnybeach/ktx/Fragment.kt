a075f32a38ee16d3c5f32334a8557fbeb34274c8
anjiemo <2695734816@qq.com>
2022-10-03 20:58:26 +0800

    优化 token 校验逻辑。

--- app/src/main/java/cn/cqautotest/sunnybeach/ktx/Fragment.kt
+++ app/src/main/java/cn/cqautotest/sunnybeach/ktx/Fragment.kt
@@ -2,16 +2,19 @@ package cn.cqautotest.sunnybeach.ktx
 
 import androidx.fragment.app.Fragment
 import cn.cqautotest.sunnybeach.model.UserBasicInfo
+import com.dylanc.longan.viewLifecycleScope
 
 /**
  * 检查是否用户登录
  * 如果没有登录则跳转至登录界面，否则执行 block Lambda 中的代码
  */
 fun Fragment.ifLoginThen(block: (userBasicInfo: UserBasicInfo) -> Unit) {
-    checkToken {
-        when (val userBasicInfo = it.getOrNull()) {
-            null -> requireActivity().tryShowLoginDialog()
-            else -> block.invoke(userBasicInfo)
+    viewLifecycleScope.launchWhenCreated {
+        checkToken {
+            when (val userBasicInfo = it.getOrNull()) {
+                null -> requireActivity().tryShowLoginDialog()
+                else -> block.invoke(userBasicInfo)
+            }
         }
     }
 }
