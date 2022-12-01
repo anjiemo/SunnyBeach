f0ccb96d63f4d31d43f8aec45abb03354297d6d6
anjiemo <2695734816@qq.com>
2022-12-01 23:31:51 +0800

    新增热门（小默）文章列表界面分享功能。

--- app/src/main/java/cn/cqautotest/sunnybeach/ui/activity/HotArticleListActivity.kt
+++ app/src/main/java/cn/cqautotest/sunnybeach/ui/activity/HotArticleListActivity.kt
@@ -4,10 +4,16 @@ import androidx.activity.viewModels
 import cn.cqautotest.sunnybeach.R
 import cn.cqautotest.sunnybeach.app.PagingActivity
 import cn.cqautotest.sunnybeach.ktx.snapshotList
+import cn.cqautotest.sunnybeach.model.UserArticle
 import cn.cqautotest.sunnybeach.ui.adapter.UserArticleAdapter
 import cn.cqautotest.sunnybeach.ui.adapter.delegate.AdapterDelegate
+import cn.cqautotest.sunnybeach.ui.dialog.ShareDialog
 import cn.cqautotest.sunnybeach.util.SUNNY_BEACH_ARTICLE_URL_PRE
 import cn.cqautotest.sunnybeach.viewmodel.ArticleViewModel
+import com.hjq.umeng.Platform
+import com.hjq.umeng.UmengShare
+import com.umeng.socialize.media.UMImage
+import com.umeng.socialize.media.UMWeb
 import kotlinx.coroutines.flow.collectLatest
 
 /**
@@ -40,5 +46,39 @@ class HotArticleListActivity : PagingActivity() {
                 BrowserActivity.start(this, url)
             }
         }
+        mHotArticleAdapter.setOnMenuItemClickListener { view, item, _ ->
+            when (view.id) {
+                R.id.ll_share -> shareArticle(item)
+                // R.id.ll_great -> articleLikes(item, position)
+            }
+        }
+        mHotArticleAdapter.setOnNineGridClickListener { sources, index ->
+            ImagePreviewActivity.start(this, sources.toMutableList(), index)
+        }
+    }
+
+    private fun shareArticle(item: UserArticle.UserArticleItem) {
+        val articleId = item.id
+        val content = UMWeb(SUNNY_BEACH_ARTICLE_URL_PRE + articleId)
+        content.title = item.title
+        content.setThumb(UMImage(this, R.mipmap.launcher_ic))
+        content.description = getString(R.string.app_name)
+        // 分享
+        ShareDialog.Builder(this)
+            .setShareLink(content)
+            .setListener(object : UmengShare.OnShareListener {
+                override fun onSucceed(platform: Platform?) {
+                    toast("分享成功")
+                }
+
+                override fun onError(platform: Platform?, t: Throwable) {
+                    toast(t.message)
+                }
+
+                override fun onCancel(platform: Platform?) {
+                    toast("分享取消")
+                }
+            })
+            .show()
     }
 }
