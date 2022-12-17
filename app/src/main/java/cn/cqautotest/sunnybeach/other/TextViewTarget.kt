9d9f2479781cf84b52a24db2099e6409baa324e2
anjiemo <2695734816@qq.com>
2022-12-17 21:53:05 +0800

    首页鱼塘新增推荐话题列表，新增话题查看界面。

---
+++ app/src/main/java/cn/cqautotest/sunnybeach/other/TextViewTarget.kt
@@ -0,0 +1,40 @@
+package cn.cqautotest.sunnybeach.other
+
+import android.graphics.drawable.Drawable
+import android.graphics.drawable.ShapeDrawable
+import android.graphics.drawable.shapes.RectShape
+import android.widget.TextView
+import androidx.annotation.Px
+import androidx.core.graphics.drawable.updateBounds
+import com.bumptech.glide.request.target.CustomViewTarget
+import com.bumptech.glide.request.transition.Transition
+
+/**
+ * author : A Lonely Cat
+ * github : https://github.com/anjiemo/SunnyBeach
+ * time   : 2022/12/17
+ * desc   : A simple implementation of TextViewTarget,
+ * usually used when you need the image and text to be aligned top and bottom, and you can specify the size of the drawable.
+ */
+class TextViewTarget(private val textView: TextView, @Px private val drawableSize: Int = 0) :
+    CustomViewTarget<TextView, Drawable>(textView) {
+
+    private val defaultDrawable = ShapeDrawable(RectShape()).apply {
+        updateBounds(right = drawableSize, bottom = drawableSize)
+    }
+
+    override fun onLoadFailed(errorDrawable: Drawable?) {
+        errorDrawable?.updateBounds(right = drawableSize, bottom = drawableSize)
+        textView.setCompoundDrawables(null, errorDrawable ?: defaultDrawable, null, null)
+    }
+
+    override fun onResourceReady(resource: Drawable, transition: Transition<in Drawable>?) {
+        resource.updateBounds(right = drawableSize, bottom = drawableSize)
+        textView.setCompoundDrawables(null, resource, null, null)
+    }
+
+    override fun onResourceCleared(placeholder: Drawable?) {
+        placeholder?.updateBounds(right = drawableSize, bottom = drawableSize)
+        textView.setCompoundDrawables(null, placeholder ?: defaultDrawable, null, null)
+    }
+}
