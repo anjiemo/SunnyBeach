178276ce3f4038bc7e7459bdd5cbcab562bd04fe
anjiemo <2695734816@qq.com>
2022-12-18 12:16:55 +0800

    引入 Palette 调色板依赖，新增自定义多色 Drawable，新增自定义圆角矩形 Drawable。

---
+++ app/src/main/java/cn/cqautotest/sunnybeach/other/RoundRectDrawable.kt
@@ -0,0 +1,36 @@
+package cn.cqautotest.sunnybeach.other
+
+import android.graphics.*
+import android.graphics.drawable.Drawable
+import androidx.annotation.ColorInt
+import androidx.annotation.Px
+import androidx.core.graphics.toRectF
+
+/**
+ * author : A Lonely Cat
+ * github : https://github.com/anjiemo/SunnyBeach
+ * time   : 2022/12/18
+ * desc   : Very simple drawable that draws a rounded rectangle background with arbitrary corners and also reports proper outline for Lollipop.
+ * Simpler and uses less resources compared to GradientDrawable or ShapeDrawable.
+ */
+class RoundRectDrawable(@Px private val radius: Int, @ColorInt private val color: Int = Color.TRANSPARENT) : Drawable() {
+
+    private val mPaint = Paint(Paint.ANTI_ALIAS_FLAG).also { it.color = color }
+
+    override fun draw(canvas: Canvas) {
+        val r = bounds.toRectF()
+        canvas.drawRoundRect(r, radius.toFloat(), radius.toFloat(), mPaint)
+    }
+
+    override fun setAlpha(alpha: Int) {
+        mPaint.alpha = alpha
+        invalidateSelf()
+    }
+
+    override fun setColorFilter(colorFilter: ColorFilter?) {
+        mPaint.colorFilter = colorFilter
+        invalidateSelf()
+    }
+
+    override fun getOpacity(): Int = PixelFormat.TRANSLUCENT
+}
