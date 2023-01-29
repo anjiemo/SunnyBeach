3b25613db206a8689d807a13c8a80a437eb784ca
anjiemo <2695734816@qq.com>
2023-01-29 23:50:34 +0800

    使用 Hilt 注入 重构分享、点赞代码逻辑，优化部分代码写法。

--- app/src/main/java/cn/cqautotest/sunnybeach/di/AppModule.kt
+++ app/src/main/java/cn/cqautotest/sunnybeach/di/AppModule.kt
@@ -1,12 +1,20 @@
 package cn.cqautotest.sunnybeach.di
 
+import android.app.Activity
+import android.content.Context
+import cn.cqautotest.sunnybeach.util.MultiOperationHelper
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ActivityComponent
+import dagger.hilt.android.qualifiers.ActivityContext
+import dagger.hilt.android.scopes.ActivityScoped
 
 @Module
 @InstallIn(ActivityComponent::class)
 object AppModule {
 
-
+    @ActivityScoped
+    @Provides
+    fun provideMultiOperationHelper(@ActivityContext ctx: Context) = MultiOperationHelper(ctx as Activity)
 }
