4924049e205f85ebac8eea084e3916c4f738c878
anjiemo <2695734816@qq.com>
2023-03-30 23:25:53 +0800

    优化消息列表工厂创建逻辑。

--- app/src/main/java/cn/cqautotest/sunnybeach/paging/source/msg/factory/QaMsgListFactory.kt
+++ app/src/main/java/cn/cqautotest/sunnybeach/paging/source/msg/factory/QaMsgListFactory.kt
@@ -11,10 +11,15 @@ import cn.cqautotest.sunnybeach.paging.source.msg.impl.IMsgPageData
  * time   : 2021/10/27
  * desc   : 问答列表消息 PagingSource 工厂
  */
-class QaMsgListFactory : AbstractMsgListFactory() {
+class QaMsgListFactory private constructor() : AbstractMsgListFactory() {
 
     @Suppress("UNCHECKED_CAST")
     override suspend fun createMsgListByType(page: Int): IApiResponse<IMsgPageData> {
         return MsgApi.getQaMsgList(page) as ApiResponse<IMsgPageData>
+    }
+
+    companion object {
+
+        fun create() = QaMsgListFactory()
     }
 }
