606b23df1124a696819a84ab4a34935b0caadd6b
anjiemo <2695734816@qq.com>
2023-06-14 23:13:22 +0800

    新增 CacheHelper 注释。

--- app/src/main/java/cn/cqautotest/sunnybeach/util/CacheHelper.kt
+++ app/src/main/java/cn/cqautotest/sunnybeach/util/CacheHelper.kt
@@ -13,6 +13,10 @@ import java.util.concurrent.TimeUnit
  * time   : 2022/05/26
  * desc   : 数据缓存帮助类，可以用于缓存网络请求的数据（一般只缓存 GET 请求返回的数据）。
  * 不建议缓存时效性较要求高的数据。
+ * 不支持缓存接口对象 或 元素为接口类型的数组和集合，否则会在反序列化的时候失败，从而获取到无效数据，此问题由 Gson 产生。如：
+ * interface Data
+ * Collection<Data>
+ * Array<Data>
  */
 object CacheHelper {
 
