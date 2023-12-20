14db570d06337b59acd1687a17f0ba946e2972ea
anjiemo <2695734816@qq.com>
2023-12-20 23:15:58 +0800

    新增邮箱掩码处理扩展函数，新增用户中心页邮箱掩码处理。

--- app/src/main/java/cn/cqautotest/sunnybeach/ktx/String.kt
+++ app/src/main/java/cn/cqautotest/sunnybeach/ktx/String.kt
@@ -3,6 +3,7 @@ package cn.cqautotest.sunnybeach.ktx
 import android.graphics.Bitmap
 import android.graphics.Color
 import com.blankj.utilcode.util.EncryptUtils
+import com.blankj.utilcode.util.RegexUtils
 import com.huawei.hms.hmsscankit.ScanUtil
 import com.huawei.hms.hmsscankit.WriterException
 import com.huawei.hms.ml.scan.HmsBuildBitmapOption
@@ -52,3 +53,14 @@ fun String.unicodeToString(): String = runCatching {
         matchResult.groupValues[1].toInt(16).toChar().toString()
     }
 }.getOrDefault(this)
+
+/**
+ * 如果当前字符串不是邮箱，则原样返回。
+ * 否则，返回掩码处理后的邮箱地址。
+ */
+fun String.maskEmail(): String {
+    // 如果不是邮箱，则返回当前字符串
+    takeUnless { RegexUtils.isEmail(this) }?.let { return this }
+    val regex = Regex("(^.{2,3})(.*)(@.*)")
+    return replace(regex) { "${it.groupValues[1]}***${it.groupValues[3]}" }
+}
