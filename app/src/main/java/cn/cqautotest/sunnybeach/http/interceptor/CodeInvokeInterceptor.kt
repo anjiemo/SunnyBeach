79d70326672abe4c924adad0191ebf22248077b9
anjiemo <2695734816@qq.com>
2025-06-26 13:32:08 +0800

    feat: 新增代码调用拦截器（方便定位请求代码）

---
+++ app/src/main/java/cn/cqautotest/sunnybeach/http/interceptor/CodeInvokeInterceptor.kt
@@ -0,0 +1,24 @@
+package cn.cqautotest.sunnybeach.http.interceptor
+
+import okhttp3.Interceptor
+import okhttp3.Response
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Invocation
+import timber.log.Timber
+
+/**
+ * author : A Lonely Cat
+ * github : https://github.com/anjiemo/SunnyBeach
+ * time   : 2025/06/26
+ * desc   : 代码调用拦截器
+ */
+class CodeInvokeInterceptor(private val loggingInterceptor: HttpLoggingInterceptor): Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val request = chain.request()
+        request.tag(Invocation::class.java)?.let { invocation ->
+            Timber.d("CodeInvokeInterceptor：===> invocation is $invocation")
+        }
+        loggingInterceptor.intercept(chain)
+        return chain.proceed(request)
+    }
+}
