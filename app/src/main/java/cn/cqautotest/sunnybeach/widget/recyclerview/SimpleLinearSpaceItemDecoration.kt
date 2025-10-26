0d6f04be11cea9c0e641a89443697750f097b0fd
anjiemo <2695734816@qq.com>
2025-10-26 10:15:08 +0800

    feat(ui): 实现二楼页面打开与关闭的状态回调及导航栏显示控制 - 新增 LiveBus事件用于处理二楼页面状态变化和返回首页功能 - 修改 RecyclerView 分割线装饰类以支持边缘间距配置

--- app/src/main/java/cn/cqautotest/sunnybeach/widget/recyclerview/SimpleLinearSpaceItemDecoration.kt
+++ app/src/main/java/cn/cqautotest/sunnybeach/widget/recyclerview/SimpleLinearSpaceItemDecoration.kt
@@ -15,8 +15,9 @@ import cn.cqautotest.sunnybeach.ktx.dp
  * desc   : RecyclerView 间距装饰（线性布局管理器）
  */
 open class SimpleLinearSpaceItemDecoration(
-    // 单位间距（实际间距的一半）
-    @param:Px private val unit: Int = 2.dp
+    // 单位间距
+    @param:Px private val unit: Int = 2.dp,
+    private val includeEdge: Boolean = true
 ) : RecyclerView.ItemDecoration() {
 
     @CallSuper
@@ -29,10 +30,26 @@ open class SimpleLinearSpaceItemDecoration(
         val layoutManager = (parent.layoutManager as? LinearLayoutManager) ?: return
         // 获取 LinearLayoutManager 的布局方向
         val orientation = layoutManager.orientation
+        val itemPosition = (view.layoutParams as RecyclerView.LayoutParams).viewLayoutPosition
+        val lastIndex = layoutManager.itemCount - 1
         if (orientation == RecyclerView.VERTICAL) {
-            outRect.bottom = unit
+            if (itemPosition == 0 && includeEdge) {
+                outRect.top = unit
+            }
+            if (itemPosition != lastIndex) {
+                outRect.bottom = unit
+            } else if (includeEdge) {
+                outRect.bottom = unit
+            }
         } else {
-            outRect.right = unit
+            if (itemPosition == 0 && includeEdge) {
+                outRect.left = unit
+            }
+            if (itemPosition != lastIndex) {
+                outRect.right = unit
+            } else if (includeEdge) {
+                outRect.right = unit
+            }
         }
     }
 }
