27b947d1765f6e60e74e792346da961e9c469535
anjiemo <2695734816@qq.com>
2025-10-26 08:44:31 +0800

    feat(refresh): 升级刷新库并添加首页摸鱼列表页二级刷新功能

--- app/src/main/java/cn/cqautotest/sunnybeach/work/CacheCleanupWorker.kt
+++ app/src/main/java/cn/cqautotest/sunnybeach/work/CacheCleanupWorker.kt
@@ -16,8 +16,7 @@ import timber.log.Timber
  * time   : 2021/11/06
  * desc   : 缓存清理 Worker
  */
-class CacheCleanupWorker(appContext: Context, workerParams: WorkerParameters) :
-    CoroutineWorker(appContext, workerParams) {
+class CacheCleanupWorker(appContext: Context, workerParams: WorkerParameters) : CoroutineWorker(appContext, workerParams) {
 
     override suspend fun doWork(): Result {
         val isAutoCleanCache = AppManager.isAutoCleanCache()
@@ -26,11 +25,11 @@ class CacheCleanupWorker(appContext: Context, workerParams: WorkerParameters) :
         Timber.d("doWork:===> start clean cache...")
         try {
             // 清除内存缓存（必须在主线程）
-            withContext(Dispatchers.Main) { GlideApp.get(applicationContext).clearMemory() }
+            withContext(Dispatchers.Main) { GlideApp[applicationContext].clearMemory() }
             withContext(Dispatchers.IO) {
                 CacheDataManager.clearAllCache(applicationContext)
                 // 清除本地缓存（必须在子线程）
-                GlideApp.get(applicationContext).clearDiskCache()
+                GlideApp[applicationContext].clearDiskCache()
             }
         } catch (t: Throwable) {
             t.printStackTrace()
