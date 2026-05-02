3eddf2b6aef4e9e0363b8bcf5c1eac1c447af8e8
anjiemo <2695734816@qq.com>
2026-05-02 22:14:38 +0800

    chore(build): 移除未使用的 Compose Gradle 插件依赖

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -26,7 +26,6 @@ dependencies {
     compileOnly(libs.android.gradlePlugin)
     compileOnly(libs.kotlin.gradlePlugin)
     compileOnly(libs.ksp.gradlePlugin)
-    compileOnly(libs.compose.gradlePlugin)
     compileOnly(libs.hilt.gradlePlugin)
 }
 
